7192bd04fc59ffa8dca5d2a09aa9c9cc521cb7d6
Dzmitry_Petukh <petuhdima@gmail.com>
2022-10-01 13:10:09 +0200

    feat:implemented all necessary requirements;

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My Manga Store!
           </Link>
         </Typography>
 
