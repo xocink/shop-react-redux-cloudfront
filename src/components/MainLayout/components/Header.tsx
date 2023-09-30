31df92fa2bce64bec87597ef1ce813d335a18005
Dzmitry_Petukh <petuhdima@gmail.com>
2023-09-30 17:06:22 +0200

    feat: task-2 implemented

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My Store Auto deployed and Invalidated!
           </Link>
         </Typography>
 
