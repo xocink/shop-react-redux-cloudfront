62317bff32f9abca735fc96977953262b05a6a8e
Dzmitry_Petukh <petuhdima@gmail.com>
2023-10-22 08:52:08 +0200

    feat: implemented taks5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://rzq3wb6sdl.execute-api.eu-west-1.amazonaws.com",
   bff: "https://vykete8wuc.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
